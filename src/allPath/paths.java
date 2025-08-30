27a4bbc9aa3aed791d2fa8bd0c4fe9b6df18fd1b
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-08-30 21:00:39 +0530

    backtrack

--- src/allPath/paths.java
+++ src/allPath/paths.java
@@ -2,6 +2,19 @@ package allPath;
 
 public class paths {
     public static void main(String[] args) {
-        System.out.println("Hi jijo");
+        path(3,3,"");
+    }
+    static void path(int r,int c,String p){
+        if (r==1 && c==1){
+            System.out.println(p);
+            return;
+        }
+        if (r > 1){
+            path(r-1,c,p+"D");
+        }
+        if (c > 1){
+            path(r,c-1,p+"R");
+        }
+
     }
 }
