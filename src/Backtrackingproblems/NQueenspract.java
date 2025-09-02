f43f6b1913491d8b8a605ec612308dace6779cda
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-09-02 22:57:45 +0530

    backtrack

--- src/Backtrackingproblems/NQueenspract.java
+++ src/Backtrackingproblems/NQueenspract.java
@@ -3,7 +3,7 @@ package Backtrackingproblems;
 public class NQueenspract {
     public static void main(String[] args) {
 
-        int n = 5;
+        int n = 4;
         boolean[][] board = new boolean[n][n];
         System.out.println(findpath(board,0));
     }
